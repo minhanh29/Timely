939deed7f860e9e65bd9c8a54884b1957bc88969
vuquang64 <61259766+vuquang64@users.noreply.github.com>
2020-11-17 21:24:05 -0800

    Add functionality to Notification Settings

--- timely/AlarmReceiver.java
+++ timely/AlarmReceiver.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.Calendar;
+
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.e("Hi","This receiver is working !");
+        Log.e("Timely","You've got a notification !!!!");
         Intent myIntent = new Intent(context, Ringtone.class);
         context.startService(myIntent);
     }
