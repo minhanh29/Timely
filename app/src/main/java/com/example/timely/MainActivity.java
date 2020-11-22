6d9edc51ee466bfdc918534207d1cb25a03d2dfa
Minh Anh <minhanhng.sofl@gmail.com>
2020-11-21 19:26:36 -0800

    Test

---
+++ app/src/main/java/com/example/timely/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.timely;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.timely.courses.StudyTime;
+import com.example.timely.settings.NotificationSettingsActivity;
+import com.example.timely.timetable.TimetableActivity;
+import com.example.timely.timetablemaker.StudyTimeActivity;
+import com.example.timely.timetablemaker.TimetableMakerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, NotificationSettingsActivity.class);
+        startActivity(intent);
+    }
+}
