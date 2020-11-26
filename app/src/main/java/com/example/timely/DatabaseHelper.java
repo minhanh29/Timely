537e4dbd8375fcfee99fad5a0b992bea7cf4c3a8
Minh Anh Nguyen <56661653+minhanh29@users.noreply.github.com>
2020-11-25 21:32:01 -0800

    Update DatabaseHelper.java

--- app/src/main/java/com/example/timely/DatabaseHelper.java
+++ app/src/main/java/com/example/timely/DatabaseHelper.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
-import android.widget.ImageView;
 
 import com.example.timely.courses.Course;
 import com.example.timely.courses.StudyTime;
