3732fcf5c53b3a431507330c646494782f5633c5
Minh Anh <minhanhng.sofl@gmail.com>
2020-11-26 01:23:05 -0800

    Design screens

--- app/src/main/java/com/example/timely/MainActivity.java
+++ app/src/main/java/com/example/timely/MainActivity.java
@@ -1,20 +1,22 @@
 package com.example.timely;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 
-import com.example.timely.courses.StudyTime;
 import com.example.timely.settings.NotificationSettingsActivity;
 import com.example.timely.timetable.TimetableActivity;
-import com.example.timely.timetablemaker.StudyTimeActivity;
 import com.example.timely.timetablemaker.TimetableMakerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     CardView timetable, generator, settings;
+    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +27,48 @@ public class MainActivity extends AppCompatActivity {
         generator = findViewById(R.id.generator_card);
         settings = findViewById(R.id.settings_card);
 
+        animation = new ScaleAnimation(1f, 1.3f, 1f, 1.3f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setDuration(105L);
+
         timetable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToTimetable();
+                v.startAnimation(animation);
+                new CountDownTimer(110, 10) {
+                    public void onFinish() {
+                        goToTimetable();
+                    }
+
+                    public void onTick(long millisUntilFinished) { }
+                }.start();
             }
         });
 
         generator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToGenerator();
+                v.startAnimation(animation);
+                new CountDownTimer(110, 10) {
+                    public void onFinish() {
+                        goToGenerator();
+                    }
+
+                    public void onTick(long millisUntilFinished) { }
+                }.start();
             }
         });
 
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToSettings();
+                v.startAnimation(animation);
+                new CountDownTimer(110, 10) {
+                    public void onFinish() {
+                        goToSettings();
+                    }
+
+                    public void onTick(long millisUntilFinished) { }
+                }.start();
             }
         });
 //        Intent intent = new Intent(this, TimetableMakerActivity.class);
