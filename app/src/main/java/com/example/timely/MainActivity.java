0925a57046fe2e6013d5f81aa72beb93910b74e2
vuquang64 <61259766+vuquang64@users.noreply.github.com>
2020-11-27 12:00:38 -0800

    Change the functionalities of the alrms

--- app/src/main/java/com/example/timely/MainActivity.java
+++ app/src/main/java/com/example/timely/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Intent intent = new Intent(this, TimetableMakerActivity.class);
+//        Intent intent = new Intent(this, NotificationSettingsActivity.class);
 //        startActivity(intent);
     }
 
