228d87053e50dec6c9e20844d711749e9f2441c8
vuquang64 <61259766+vuquang64@users.noreply.github.com>
2020-11-30 15:53:48 -0800

    edit notification functionalities

--- app/src/main/java/com/example/timely/settings/AlarmReceiver.java
+++ app/src/main/java/com/example/timely/settings/AlarmReceiver.java
@@ -5,92 +5,42 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.provider.AlarmClock;
-import android.util.Log;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
-import com.example.timely.DatabaseHelper;
-import com.example.timely.courses.StudyTime;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
+import com.example.timely.R;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.e("Timely","You've got a notification !!!!");
-//        Intent myIntent = new Intent(context, Ringtone.class);
-//        context.startService(myIntent);
-        Intent mIntent = new Intent(context, NotificationSettingsActivity.class);
-        mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        mIntent.putExtra(NotificationSettingsActivity.UPDATE_ALARM, true);
-        context.startActivity(mIntent);
-        //startNextAlarm(context);
-    }
+        String message="You've got a notification";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"notifyTimely")
+                .setContentText(message)
+                .setContentTitle("Timely Alarm")
+                .setSmallIcon(R.drawable.ic_baseline_alarm_24)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-    public void startNextAlarm(Context context)
-    {
-        int[] time = getNextTime(context);
-        Log.i("time", "Day:" + time[0] + "hour: " + time[1] + "minute: " + time[2]);
-
-        Intent intent2 = new Intent(context, AlarmReceiver.class);
-        intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pi =PendingIntent.getBroadcast(context, 1,intent2, 0);
-
-        Calendar c = Calendar.getInstance();
-        //c.set(Calendar.DAY_OF_WEEK, time[0]);
-        c.set(Calendar.HOUR_OF_DAY, time[1]);
-        c.set(Calendar.MINUTE, time[2]);
-
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pi);
-
-        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        ArrayList<Integer> days = new ArrayList<>();
-        days.add(time[0]);
-        intent.putExtra(AlarmClock.EXTRA_DAYS, days);
-        intent.putExtra(AlarmClock.EXTRA_HOUR, time[1]);
-        intent.putExtra(AlarmClock.EXTRA_MINUTES, time[2]);
-
-        context.startActivity(intent);
-
-    }
-
-
-    public int[] getNextTime(Context context)
-    {
-        int[] time = new int[3];
-        DatabaseHelper db = new DatabaseHelper(context);
-        ArrayList<StudyTime> studyTimes = db.getAllStudyTime();
-        Collections.sort(studyTimes);
-
-        Calendar calendar = Calendar.getInstance();
-        StudyTime minTime = new StudyTime(calendar.get(Calendar.DAY_OF_WEEK)-2,
-                calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), 0, "");
-        boolean change = false;
-        for (int i = 0; i < studyTimes.size(); i++)
-        {
-            if (minTime.compareTo(studyTimes.get(i)) < 0)
-            {
-                minTime = studyTimes.get(i);
-                change = true;
-                break;
-            }
-        }
-
-        // this is the end of the week
-        // find the min time of next week
-        if (!change)
-        {
-            minTime = studyTimes.get(0);
-        }
-
-        time[0] = minTime.getDay()+2;
-        time[1] = minTime.getHour();
-        time[2] = minTime.getMinute();
-
-        return time;
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(0, builder.build());
     }
 }
+
+//    NotificationCompat.Builder builder = new NotificationCompat.Builder(
+//            Ringtone.this
+//    )
+//            .setContentText(message)
+//            .setContentTitle("Timely Alarm")
+//            .setSmallIcon(R.drawable.ic_baseline_alarm_24)
+//            .setAutoCancel(true);
+//
+//    Intent newIntent = new Intent(Ringtone.this, NotificationActivity.class);
+//        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                newIntent.putExtra("message", message);
+//
+//                PendingIntent pendingIntent = PendingIntent.getActivity(Ringtone.this,0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//                builder.setContentIntent(pendingIntent);
+//
+//                NotificationManager notificationManager = (NotificationManager)getSystemService(
+//                Context.NOTIFICATION_SERVICE
+//                );
+//                notificationManager.notify(0, builder.build());
--- app/src/main/java/com/example/timely/settings/Ringtone.java
+++ app/src/main/java/com/example/timely/settings/Ringtone.java
@@ -1,28 +1,24 @@
 package com.example.timely.settings;
 
-import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-import android.provider.AlarmClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
-import com.example.timely.DatabaseHelper;
 import com.example.timely.R;
-import com.example.timely.courses.StudyTime;
-
-import java.security.Provider;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
+import static com.example.timely.settings.App.CHANNEL_TIMELY_ID;
 
 public class Ringtone extends Service {
     MediaPlayer mediaPlayer;
+    private NotificationManager notificationManager;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -31,17 +27,42 @@ public class Ringtone extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
         mediaPlayer = MediaPlayer.create(this, R.raw.alarm_sound);
         mediaPlayer.start();
         Log.e("Hi this music is running", "Lalala");
 
+        //Create an Intent for the BroadcastReceiver
+        Intent buttonIntent = new Intent(this, ButtonReceiver.class);
+        buttonIntent.putExtra("notificationId",2);
+        buttonIntent.putExtra("selected_sound", R.raw.alarm_sound);
+
+        //Create the PendingIntent
+        PendingIntent btPendingIntent = PendingIntent.getBroadcast(this, 0, buttonIntent,0);
+
+        // start a notification
+        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_TIMELY_ID)
+                .setSmallIcon(R.drawable.ic_baseline_alarm_24)
+                .setContentTitle("Timely Alarm")
+                .setContentText("You've got an alarm")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .addAction(R.drawable.ic_baseline_alarm_off_24,"Dismiss Alarm", btPendingIntent)
+                .build();
+        Log.e("Timely", "This starts the nortifications");
+        startForeground(2,notification);
+
+
         // set the next alarm
         Intent mIntent = new Intent(this, AlarmService.class);
         startService(mIntent);
+
 
         // stop this ringing service
         stopSelf();
 
         return START_NOT_STICKY;
     }
+
 }
